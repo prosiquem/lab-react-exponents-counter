0635cc2d4b00ca4667b97dc87c96755e05f514b1
Pedro Rosique <rosiquemurciapedro@gmail.com>
2024-11-07 18:25:44 +0100

    states lifted up

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
 import ExponentTwo from "./components/ExponentTwo";
@@ -6,23 +7,27 @@ import ExponentFour from "./components/ExponentFour";
 import ExponentFive from "./components/ExponentFive";
 import ExponentSix from "./components/ExponentSix";
 
+function App() {
+  const [count, setCount] = useState(0);
 
-function App () {
+  const decrement = () => setCount((prevCount) => prevCount - 1);
+  const increment = () => setCount((prevCount) => prevCount + 1);
+
   return (
     <div className="App">
       <h2><em>Counter</em></h2>
-  
-      <Counter/>
+
+      <Counter count={count} increment={increment} decrement={decrement} />
 
       <br />
       <h2><em>Exponents</em></h2>
 
       <div className="container">
-        <ExponentTwo />
-        <ExponentThree />
-        <ExponentFour />
-        <ExponentFive />
-        <ExponentSix />
+        <ExponentTwo count={count} />
+        <ExponentThree count={count} />
+        <ExponentFour count={count} />
+        <ExponentFive count={count} />
+        <ExponentSix count={count} />
       </div>
     </div>
   );
--- src/components/ExponentTwo.jsx
+++ src/components/ExponentTwo.jsx
@@ -1,8 +1,15 @@
-const ExponentTwo = () => (
-   <div className="exponent-counter-container">
-    <p className="exponent-label">n²</p>
-    <p className="exponent-result">2 * 2 = <span className="total">4</span></p>
-  </div>
-);
+const ExponentTwo = ({ count }) => {
+
+  const result = count * count
+
+  return (
+    <div className="exponent-counter-container">
+      <p className="exponent-label">{count}²</p>
+      <p className="exponent-result">
+        {count} * {count} = <span className="total">{result}</span>
+      </p>
+    </div>
+  )
+};
 
 export default ExponentTwo;
